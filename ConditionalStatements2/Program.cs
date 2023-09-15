33c2af180fa8aac7084279696e98e726875955ea
e.vladimirova24 <e.vladimirova24@LAB108PC02>
2023-09-15 09:34:44 +0300

    Add project files.

---
+++ ConditionalStatements2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("#3");
+Console.WriteLine("Choose a number?");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 0)
+{
+    Console.WriteLine("Positive");
+}
+else
+{
+    Console.WriteLine("Negative");
+}
